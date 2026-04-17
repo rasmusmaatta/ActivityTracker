9a187bfa7c854bc8720268f567cd294758fdcda7
= rasmusmaatta <= maatta.rasmus@gmail.com>
2026-04-17 14:49:52 +0300

    muokkauksia

--- publish.sh
+++ publish.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd target
-../mvnw package -DskipTests
-scp activitytracker-0.0.1-SNAPSHOT.jar rasmusm@softala.haaga-helia.fi:
-
+./mvnw package -DskipTests
+scp target/activitytracker-0.0.1-SNAPSHOT.jar rasmusm@softala.haaga-helia.fi:
 ssh rasmusm@softala.haaga-helia.fi "java -jar activitytracker-0.0.1-SNAPSHOT.jar"
